3a241f1b0e88823d16264ff6f0650954b6b1456f
SaraSchim <sarashim653@gmail.com>
2021-07-14 14:16:00 +0300

    find all lines

--- initialize_database.py
+++ initialize_database.py
@@ -19,12 +19,3 @@ def find_all_lines(root_path):
 path = '2021-archive'
 find_all_lines(path)
 
-# files = []
-# # r=root, d=directories, f = files
-# for r, d, f in os.walk(path):
-#     for file in f:
-#         if '.txt' in file:
-#             files.append(os.path.join(r, file))
-#
-# for f in files:
-#     print(f)
