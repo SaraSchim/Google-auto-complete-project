a9aab16fdc8b62739a1a746cda7d9231340c61dd
SaraSchim <sarashim653@gmail.com>
2021-07-15 00:10:22 +0300

    s

--- search.py
+++ search.py
@@ -2,11 +2,11 @@ import json
 import re
 
 
-
 def get_data():
     with open('database.json', "r") as DB_file:
-        data_base=json.load(DB_file)
+        data_base = json.load(DB_file)
     return data_base
+
 
 def get_lines():
     with open('database_lines.json', "r") as DB_file:
@@ -14,37 +14,36 @@ def get_lines():
     return lines
 
 
+lines = get_lines()
+data_base = get_data()
 
-lines=get_lines()
-data_base=get_data()
 
 #########################
 
 
-
 def search(sentence):
-    num_result_sentence=0
+    num_result_sentence = 0
     current_node = data_base
     regex = re.compile('[^a-zA-Z\s]')
-    sentence=regex.sub('', sentence)
+    sentence = regex.sub('', sentence)
     sentence = sentence.lower()
-    sentence ="".join(sentence.split(" "))
+    sentence = "".join(sentence.split(" "))
 
     for letter in sentence:
         if current_node.get(letter) is None:
             for i in current_node.keys():
                 pass
-            finish=0
+            finish = 0
             break
         current_node = current_node[letter]
     if finish:
-        index_list=get_index_sentence(current_node[" "])
-        if len(index_list)==5:
+        index_list = get_index_sentence(current_node[" "])
+        if len(index_list) == 5:
             parse_and_sort(index_list)
             return
-        num_result_sentence=len(index_list)
-    for i in range(len(sentence),0,-1):
-        sentence=sentence[i]="*"
+        num_result_sentence = len(index_list)
+    for i in range(len(sentence), 0, -1):
+        sentence = sentence[i] = "*"
 
 
 def get_index_sentence(node):
@@ -82,12 +81,17 @@ def parse_and_sort(sentences_list):
 
 def add_char(sentence):
     for char in range(len(sentence), 0, -1):
-        fixed_sentence = sentence.remove(char)
+        fixed_sentence = sentence.remove(sentence[char])
         search(fixed_sentence)
 
 
-def bb(current_node,list=[]):
+def remove_char(sentence):
+    for char in range(len(sentence), 0, -1):
+        fixed_sentence = sentence[char] = "*"
+        search(fixed_sentence)
+
+
+def bb(current_node, list=[]):
     for i in current_node.keys():
         if current_node.get(" ") is None:
-            bb(current_node[i],list)
-
+            bb(current_node[i], list)
