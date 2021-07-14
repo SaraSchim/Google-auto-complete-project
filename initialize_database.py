700c0a06e91f905ce908da28f69cb8781adea86c
SaraSchim <sarashim653@gmail.com>
2021-07-14 22:42:41 +0300

    search

--- initialize_database.py
+++ initialize_database.py
@@ -1,24 +1,39 @@
+import bisect
 import json
 import os
 import re
 
 data_base = dict()
 lines = []
+file_name = 1
 
 
 def insert(line, line_index):
+    global file_name
     line = line.lower()
     line = line.split()
     for idx_first_word in range(len(line)):
         current_node = data_base
         for word in line[idx_first_word:len(line)]:
-            for latter in word:
-                if current_node.get(latter) is None:
-                    current_node[latter] = dict()
-                current_node = current_node[latter]
+            for letter in word:
+                if current_node.get(letter) is None:
+                    current_node[letter] = dict()
+                current_node = current_node[letter]
             if current_node.get(" ") is None:
                 current_node[" "] = []
-            current_node[" "].append(line_index)
+                # current_node[" "] = "{}.txt".format(file_name)
+                # print(word,file_name,line_index)
+            if len(current_node[" "]) < 10:
+                current_node[" "].append(line_index)
+            elif len(current_node[" "]) == 11:
+                with open("DB_files\{}".format(file_name), "a") as sen_file:
+                    for i in current_node[" "]:
+                        sen_file.write("{}\n".format(i))
+                current_node[" "] = "{}.txt".format(file_name)
+                file_name += 1
+            else:
+                with open("DB_files\{}".format(current_node[" "]), "a") as sen_file:
+                    sen_file.write("{}\n".format(line_index))
 
 
 def find_all_lines(root_path):
@@ -28,24 +43,25 @@ def find_all_lines(root_path):
         for file in f:
             if '.txt' in file:
                 print(count)
-                print(os.path.join(r, file))
+                # print(os.path.join(r, file))
                 count += 1
                 with open(os.path.join(r, file), "r", encoding="cp437", errors='ignore') as txt_file:
                     for line in txt_file.read().splitlines():
                         regex = re.compile('[^a-zA-Z\s]')
-                        line=regex.sub('', line)
+                        line = regex.sub('', line)
+
                         if len(line) >= 2:
                             lines.append(line)
-                            insert(line, len(lines)-1)
+                            insert(line, len(lines) - 1)
 
 
 def write_DB_to_file():
-    path = '1'
+    path = '2021-archive'
     find_all_lines(path)
     with open('database.json', "w") as DB_file:
-         json.dump(data_base, DB_file)
+        json.dump(data_base, DB_file)
     with open('database_lines.json', "w") as DB_file:
-         json.dump(lines, DB_file)
+        json.dump(lines, DB_file)
 
 
 write_DB_to_file()
--- search.py
+++ search.py
@@ -1,6 +1,8 @@
 import json
 import re
 
+
+
 def get_data():
     with open('database.json', "r") as DB_file:
         data_base=json.load(DB_file)
@@ -10,27 +12,28 @@ def get_lines():
     with open('database_lines.json', "r") as DB_file:
         lines = json.load(DB_file)
     return lines
-def change(sentence):
-    for letter in sentence[::-1]:
-        if current_node.get(letter) is None:
-            if not change(sentence):
-                return
-        l
-        current_node = current_node[letter]
-    for i in range(len(current_node[" "])):
-        print(i + 1, ".", lines[current_node[" "][i]])
+
+
+
+lines=get_lines()
+data_base=get_data()
+
+#########################
+
+
+
 def search(sentence):
     num_result_sentence=0
     current_node = data_base
     regex = re.compile('[^a-zA-Z\s]')
-    sentence=regex.sub('', sen)
+    sentence=regex.sub('', sentence)
     sentence = sentence.lower()
     sentence ="".join(sentence.split(" "))
 
     for letter in sentence:
         if current_node.get(letter) is None:
             for i in current_node.keys():
-
+                pass
             finish=0
             break
         current_node = current_node[letter]
@@ -43,13 +46,48 @@ def search(sentence):
     for i in range(len(sentence),0,-1):
         sentence=sentence[i]="*"
 
+
+def get_index_sentence(node):
+    if type(node) == list:
+        if len(node) >= 5:
+            result_list = node[:5]
+            return result_list
+        else:
+            return node
+    else:
+        with open(node, "r") as file:
+            result_list = file.readlines()
+            if len(result_list) >= 5:
+                return result_list[:5]
+            else:
+                return result_list
+
+
+def change(sentence):
+    for letter in sentence[::-1]:
+        if current_node.get(letter) is None:
+            if not change(sentence):
+                return
+
+        current_node = current_node[letter]
+
+    return current_node[" "]
+
+
+def parse_and_sort(sentences_list):
+    result_list = [lines[i] for i in sentences_list]
+    result_list.sort()
+    return result_list
+
+
+def add_char(sentence):
+    for char in range(len(sentence), 0, -1):
+        fixed_sentence = sentence.remove(char)
+        search(fixed_sentence)
+
+
 def bb(current_node,list=[]):
     for i in current_node.keys():
         if current_node.get(" ") is None:
             bb(current_node[i],list)
 
-
-# lines=get_lines()
-# data_base=get_data()
-# sentence = input("please enter sentence:")
-# search(sentence)
