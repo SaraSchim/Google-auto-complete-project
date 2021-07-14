6cedc376f8ee70ff8ed9e2b4c0804632bcbc59b9
SaraSchim <sarashim653@gmail.com>
2021-07-14 14:38:32 +0300

    find all lines

--- initialize_database.py
+++ initialize_database.py
@@ -7,15 +7,22 @@ def find_all_lines(root_path):
     for r, d, f in os.walk(root_path):
         for file in f:
             if '.txt' in file:
-                with open(os.path.join(r, file),"r", encoding="cp437", errors='ignore') as txt_file:
-                    # print(txt_file.read().splitlines())
-                    # print(txt_file.readlines())
-                    for line in txt_file.read().splitlines():
-                        if line != "":
-                            lines.append(line)
+                with open(os.path.join(r, file), "r") as txt_file:
+                    print(txt_file.read().splitlines())
+                    # for line in txt_file.read().splitlines():
+                    #     lines.append(line)
     print(lines)
 
 
 path = '2021-archive'
 find_all_lines(path)
 
+# files = []
+# # r=root, d=directories, f = files
+# for r, d, f in os.walk(path):
+#     for file in f:
+#         if '.txt' in file:
+#             files.append(os.path.join(r, file))
+#
+# for f in files:
+#     print(f)
