aab7d78fdd90e17b51b4bc3cc52d39d4da777be0
SaraSchim <sarashim653@gmail.com>
2021-07-15 01:51:55 +0300

    search

--- search.py
+++ search.py
@@ -2,11 +2,11 @@ import json
 import re
 
 
-
 def get_data():
     with open('database.json', "r") as DB_file:
-        data_base=json.load(DB_file)
+        data_base = json.load(DB_file)
     return data_base
+
 
 def get_lines():
     with open('database_lines.json', "r") as DB_file:
@@ -14,45 +14,57 @@ def get_lines():
     return lines
 
 
+lines = get_lines()
+data_base = get_data()
 
-lines=get_lines()
-data_base=get_data()
 
 #########################
+
+def find_node_by_sentence(sentence):
+    node = data_base
+    for i in sentence:
+        node = node.get(i)
+        if not node:
+            return None
+    return node.get(" ")
+
+print(find_node_by_sentence("is"))
 
 
 
 def search(sentence):
-    num_result_sentence=0
+    num_result_sentence = 0
     current_node = data_base
     regex = re.compile('[^a-zA-Z\s]')
-    sentence=regex.sub('', sentence)
+    sentence = regex.sub('', sentence)
     sentence = sentence.lower()
-    sentence ="".join(sentence.split(" "))
+    sentence = "".join(sentence.split(" "))
 
     for letter in sentence:
         if current_node.get(letter) is None:
             for i in current_node.keys():
                 pass
-            finish=0
+            finish = 0
             break
         current_node = current_node[letter]
     if finish:
-        index_list=get_index_sentence(current_node[" "])
-        if len(index_list)==5:
+        index_list = get_index_sentence(current_node[" "])
+        if len(index_list) == 5:
             parse_and_sort(index_list)
             return
-        num_result_sentence=len(index_list)
-    for i in range(len(sentence),0,-1):
-        sentence=sentence[i]="*"
+        num_result_sentence = len(index_list)
+    for i in range(len(sentence), 0, -1):
+        sentence = sentence[i] = "*"
+
 
 def get_index_sentence(aa):
-    print("get_index_sentence",aa)
+    print("get_index_sentence", aa)
     return aa
 
 
 def parse_and_sort(aa):
-    print("parse_and_sort",aa)
+    print("parse_and_sort", aa)
+
 
 # def get_index_sentence(node):
 #     if type(node) == list:
@@ -92,7 +104,8 @@ def add_char(sentence):
         fixed_sentence = sentence.remove(char)
         search(fixed_sentence)
 
-list=[]
+
+list = []
 def go_down_db(current_node):
     global list
     for i in current_node.keys():
@@ -100,17 +113,25 @@ def go_down_db(current_node):
             go_down_db(current_node[i])
         else:
             index_list = get_index_sentence(current_node[" "])
-            list=set(list+index_list)
-            list=[i for i in list]
-            if len(list)>=5:
+            list = set(list + index_list)
+            list = [i for i in list]
+            if len(list) >= 5:
                 print(list)
                 parse_and_sort(list)
                 exit()
+
 
 # lines=get_lines()
 # data_base=get_data()
 # sentence = input("please enter sentence:")
 # search(sentence)
 
-z={'t': {'h': {'i': {'s': {' ': [1, 2, 3, 4, 5], 'i': {'s': {' ': [1, 2, 3, 6], 'c': {'a': {'t': {' ': [1]}}}, 'm': {'e': {'l': {'l': {'o': {'n': {' ': [2, 6]}}}}}}}}, 'm': {'e': {'l': {'l': {'o': {'n': {' ': [4]}}}}}}}}}}, 'i': {'s': {' ': [1, 2, 3], 'c': {'a': {'t': {' ': [1]}}}, 'm': {'e': {'l': {'l': {'o': {'n': {' ': [2]}}}}}}}}, 'c': {'a': {'t': {' ': [1]}}}, 'm': {'e': {'l': {'l': {'o': {'n': {' ': [2, 4, 6]}}}}}}, 'a': {'h': {'i': {'s': {' ': [7]}}}}}
+z = {'t': {'h': {'i': {'s': {' ': [1, 2, 3, 4, 5], 'i': {
+    's': {' ': [1, 2, 3, 6], 'c': {'a': {'t': {' ': [1]}}}, 'm': {'e': {'l': {'l': {'o': {'n': {' ': [2, 6]}}}}}}}},
+                             'm': {'e': {'l': {'l': {'o': {'n': {' ': [4]}}}}}}}}}},
+     'i': {'s': {' ': [1, 2, 3], 'c': {'a': {'t': {' ': [1]}}}, 'm': {'e': {'l': {'l': {'o': {'n': {' ': [2]}}}}}}}},
+     'c': {'a': {'t': {' ': [1]}}}, 'm': {'e': {'l': {'l': {'o': {'n': {' ': [2, 4, 6]}}}}}},
+     'a': {'h': {'i': {'s': {' ': [7]}}}}}
 go_down_db(z)
+
+
