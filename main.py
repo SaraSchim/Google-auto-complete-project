46fe4be73848a74dd455c759b68b9682206e1305
noa-cohen-Israel <n4127987@gmail.com>
2021-07-14 16:42:05 +0300

    search

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello")
+
+data_base=dict()
+def sereach():
+    current_node = data_base
+    sen=input("please enter senteance:")
+    sen = sen.lower()
+    sen = sen.split()
+    for letter in sen:
+        if current_node.get(letter) is None:
+            print("nothing")
+        current_node = current_node[letter]
+    return current_node[" "]
